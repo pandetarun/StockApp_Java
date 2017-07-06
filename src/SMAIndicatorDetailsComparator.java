76bbc92373dbf810959b1dc08564c5b2fbed5798
rishit <rishit@rishit-PC>
2017-07-06 23:07:35 +0530

    Comparator logic modified to include additional attributes

--- src/SMAIndicatorDetailsComparator.java
+++ src/SMAIndicatorDetailsComparator.java
@@ -4,22 +4,26 @@ public class SMAIndicatorDetailsComparator implements Comparator<SMAIndicatorDet
 
 	@Override
 	public int compare(SMAIndicatorDetails obj1, SMAIndicatorDetails obj2) {
+		//Comaprison for first object SMAtoSMA not null and Buy and second object SMAtoSMA null or not buy
 		if(obj1.signalSMAToSMA!=null && obj1.signalSMAToSMA.equalsIgnoreCase("buy") && (obj2.signalSMAToSMA==null || !obj2.signalSMAToSMA.equalsIgnoreCase("buy"))){
 			return -1;
 		}
-
+		//Comaprison for first object SMAtoSMA not null and  Buy and second object SMAtoSMA not null and put
 		if(obj1.signalSMAToSMA!=null && obj1.signalSMAToSMA=="buy" && (obj2.signalSMAToSMA!=null || obj2.signalSMAToSMA.equalsIgnoreCase("put"))) {
 			return -1;
 		}
-
-		if(obj1.signalSMAToSMA!=null && obj1.signalSMAToSMA.equalsIgnoreCase("buy") && (obj2.signalSMAToSMA!=null || obj2.signalSMAToSMA.equalsIgnoreCase("buy"))) {
-			if(obj1.signalPriceToSMA!=null && obj1.signalPriceToSMA.equalsIgnoreCase("buy") && (obj2.signalPriceToSMA==null || obj2.signalPriceToSMA.equalsIgnoreCase("put"))) {
+		//Comaprison for first object SMAtoSMA not null and  Buy and second object SMAtoSMA not null and buy
+		if(obj1.signalSMAToSMA!=null && obj1.signalSMAToSMA.equalsIgnoreCase("buy") && obj2.signalSMAToSMA!=null && obj2.signalSMAToSMA.equalsIgnoreCase("buy")) {
+			if(obj1.SMAToSMApercentageDeviation > obj2.SMAToSMApercentageDeviation) {
 				return -1;
 			}
-		}
-		
-		if(obj1.signalSMAToSMA!=null && obj1.signalSMAToSMA.equalsIgnoreCase("buy") && obj2.signalSMAToSMA!=null && obj2.signalSMAToSMA.equalsIgnoreCase("buy")) {
+			if(obj1.signalPriceToSMA!=null && obj1.signalPriceToSMA.equalsIgnoreCase("buy") && (obj2.signalPriceToSMA==null || obj2.signalPriceToSMA.equalsIgnoreCase("put"))) {
+				return -1;
+			}			
 			if(obj1.signalPriceToSMA!=null && obj1.signalPriceToSMA.equalsIgnoreCase("buy") && obj2.signalPriceToSMA!=null && obj2.signalPriceToSMA.equalsIgnoreCase("buy")) {
+				if(obj1.priceToSMApercentageDeviation > obj2.priceToSMApercentageDeviation) {
+					return -1;
+				}
 				if(obj1.percentagePriceChange>obj2.percentagePriceChange) {
 					return -1;
 				}
@@ -35,6 +39,9 @@ public class SMAIndicatorDetailsComparator implements Comparator<SMAIndicatorDet
 		}
 		
 		if(obj1.signalSMAToSMA==null && obj1.signalPriceToSMA.equalsIgnoreCase("buy") && obj2.signalSMAToSMA==null && obj2.signalPriceToSMA!=null && obj2.signalPriceToSMA.equalsIgnoreCase("buy")) {
+			if(obj1.priceToSMApercentageDeviation > obj2.priceToSMApercentageDeviation) {
+				return -1;
+			}
 			if(obj1.percentagePriceChange>obj2.percentagePriceChange) {
 				return -1;
 			}
