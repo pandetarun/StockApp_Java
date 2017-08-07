2d3638e097a5281f978da81d8b3bf95a817ef7df
rishit <rishit@rishit-PC>
2017-08-07 22:35:22 +0530

    Update

---
+++ src/OnBalanceVolumeData.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class OnBalanceVolumeData {
+	public String stockName;
+	public ArrayList<String> tradeddate;
+	public ArrayList<Float> closePrice;
+	public ArrayList<Long> volume;
+	public ArrayList<Long> onBalanceVolume;
+	float percentageChange;
+}
