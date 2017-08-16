74d39931eeb23ab26c6f7aa27ed3ff51cd78e79b
rishit <rishit@rishit-PC>
2017-08-16 09:12:44 +0530

    OnBalanaceVolume

---
+++ src/OnBalanceVolumeIndicator.java
@@ -0,0 +1,9 @@
+
+public class OnBalanceVolumeIndicator {
+	public String stockName;
+	public String tradeddate;
+	public Long volume;
+	public Long volumeChangeInLastDay;
+	float percentageChangeInLastDay;
+	public boolean continuousIncreasedVolume;
+}
