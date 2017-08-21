3152a35603137e208d8c8d3f03272f23ebec05fa
tarun.pandey <tarun.pandey@PDC2LAP-6020G31.dir.svc.accenture.com>
2017-08-21 20:57:42 +0530

    Updated to save excel

--- src/UpdateIndicatedStocks.java
+++ src/UpdateIndicatedStocks.java
@@ -1,12 +1,67 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class UpdateIndicatedStocks {
-
+	private final static String PATH_TO_FILE = "C:\\Tarun\\Personal\\Tool\\StockApp";
+	private final static String NAME_OF_FILE = "SuggestedStockList.xlsx";
+	StockExcelData tmpStockExcelData = null;
+	int excelLastRow;
+	
+	
 	public static void main(String[] args) {
 		
 	}
 	
 	public void updateSMAIndication(ArrayList<SMAIndicatorDetails> SMAIndicatorDetailsList) {
+		XSSFWorkbook outputWorkbook = null;		
+		outputWorkbook = ExcelHandler.createReadWriteWorkBook(PATH_TO_FILE, NAME_OF_FILE);
+		int counter = 1;
+		int existinigStockRow;
+		int todaysColumn;
+		
+		try {
+			todaysColumn = identifyTodayColumninExcel(outputWorkbook, "SMA");
+			readExcelData(outputWorkbook, "SMA");
+			DateFormat dateFormat = new SimpleDateFormat("dd-MMM-yyyy");
+			String todayDate = dateFormat.format(new Date());
+			for (SMAIndicatorDetails tmpSMAIndicatorDetails : SMAIndicatorDetailsList) {
+				if(counter<=20) {
+					//stock already there in excel
+					if(tmpStockExcelData.stockName.contains(tmpSMAIndicatorDetails.stockCode)) {
+						existinigStockRow = tmpStockExcelData.stockName.indexOf(tmpSMAIndicatorDetails.stockCode);
+						existinigStockRow = existinigStockRow + 2;
+						//tmpStockExcelData.excelRow.get(existinigStockRow);
+						//tmpStockExcelData.repeatTime.add(existinigStockRow, tmpStockExcelData.repeatTime.get(existinigStockRow) + 1);
+						//put repeat counter in excel
+						ExcelHandler.setCellValueX(outputWorkbook, "SMA", existinigStockRow, 4, tmpStockExcelData.repeatTime.get(existinigStockRow) + 1+"");
+						//Enter price in todays date column
+						ExcelHandler.setCellValueX(outputWorkbook, "SMA", existinigStockRow, todaysColumn,tmpSMAIndicatorDetails.stockPrice+"");
+						//Enter Date
+						ExcelHandler.setCellValueX(outputWorkbook, "SMA", existinigStockRow, 2,todayDate);
+						//Enter rank
+						ExcelHandler.setCellValueX(outputWorkbook, "SMA", existinigStockRow, 2,counter+"");
+						
+					} else {
+						//Stock not there in excel. Create entry at lst row
+						ExcelHandler.setCellValueX(outputWorkbook, "SMA", excelLastRow, 1, tmpSMAIndicatorDetails.stockCode);
+						//DateFormat dateFormat = new SimpleDateFormat("dd-MMM-yyyy");
+						ExcelHandler.setCellValueX(outputWorkbook, "SMA", excelLastRow, 2, dateFormat.format(new Date()));
+						ExcelHandler.setCellValueX(outputWorkbook, "SMA", excelLastRow, 3, counter+"");
+						ExcelHandler.setCellValueX(outputWorkbook, "SMA", excelLastRow, 4, 0+"");
+					}
+					counter++;
+				} else {
+					break;
+				}
+			}
+			outputWorkbook.close();
+		} catch (Exception ex) {
+			
+		}
 		
 	}
 	
@@ -20,5 +75,51 @@ public class UpdateIndicatedStocks {
 	
 	public void updateATRIndication(ArrayList<SMAIndicatorDetails> SMAIndicatorDetailsList) {
 		
+	}
+	
+	private void readExcelData(XSSFWorkbook outputWorkbook, String sheetName) {
+		tmpStockExcelData = new StockExcelData();
+		ArrayList<String> stockName = new ArrayList<String>();
+		ArrayList<Integer> repeatTime = new ArrayList<Integer>();
+		ArrayList<Integer> excelRow = new ArrayList<Integer>();
+		String cellValue;
+		
+		try {
+			for ( int row = 3;; row++) {
+				//stockName
+				cellValue = ExcelHandler.getCellValueX(outputWorkbook, sheetName, row, 1);
+				if (cellValue!=null && !cellValue.isEmpty() && !cellValue.equalsIgnoreCase("")) {
+					stockName.add(cellValue);
+					//repeattime
+					cellValue = ExcelHandler.getCellValueX(outputWorkbook, sheetName, row, 4);
+					repeatTime.add(Integer.parseInt(cellValue));
+					//excelRow
+					excelRow.add(row);
+				} else {
+					excelLastRow = row;
+					break;
+				}
+			}
+			tmpStockExcelData.excelRow = excelRow;
+			tmpStockExcelData.repeatTime = repeatTime;
+			tmpStockExcelData.stockName = stockName;
+		} catch (Exception ex) {
+			
+		}
+	}
+	
+	private int identifyTodayColumninExcel(XSSFWorkbook outputWorkbook, String sheetName) throws Exception{
+		DateFormat dateFormat = new SimpleDateFormat("dd-MMM-yyyy");
+		String todayDate = dateFormat.format(new Date());
+		String cellValue;
+		
+		for (int colCounter = 5;;colCounter++ ) {
+			cellValue = ExcelHandler.getCellValueX(outputWorkbook, sheetName, 3, colCounter);
+			if(todayDate.equalsIgnoreCase(cellValue)) {
+				return colCounter;
+			} else if(colCounter>100) {
+				return 0;
+			}
+		}
 	}
 }
