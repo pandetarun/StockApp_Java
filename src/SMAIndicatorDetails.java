1cea3feb7e72004f6092e934d98667b6797d25b6
tarun.pandey <tarun.pandey@PDC2LAP-6020G31.dir.svc.accenture.com>
2017-08-24 08:00:44 +0530

    Updated

--- src/SMAIndicatorDetails.java
+++ src/SMAIndicatorDetails.java
@@ -2,6 +2,7 @@ import java.time.LocalDate;
 
 public class SMAIndicatorDetails {// implements Comparable<SMAIndicatorDetails> {
 	String stockCode;
+	double stockPrice;
 	boolean pricegrowth = false;
 	float percentagePriceChange;
 	String signalPriceToSMA; //"put" for options in case stock dropping down. "buy" in case price going up and crossed middleperiodSMA
@@ -13,100 +14,5 @@ public class SMAIndicatorDetails {// implements Comparable<SMAIndicatorDetails> 
 	boolean SMNSMcrossover = false;
 	boolean PNSMcontinuousGrowth = false;
 	boolean SMNSMcontinuousGrowth = false;
-	
-	/*public int compareTo(SMAIndicatorDetails other)
-	  {
-		if(signalSMAToSMA!=null && signalSMAToSMA.equalsIgnoreCase("buy") && (other.signalSMAToSMA==null || !other.signalSMAToSMA.equalsIgnoreCase("buy"))){
-			return 1;
-		}
-
-		if(signalSMAToSMA!=null && signalSMAToSMA=="buy" && (other.signalSMAToSMA!=null || other.signalSMAToSMA.equalsIgnoreCase("put"))) {
-			return 1;
-		}
-
-		if(signalSMAToSMA!=null && signalSMAToSMA.equalsIgnoreCase("buy") && (other.signalSMAToSMA!=null || other.signalSMAToSMA.equalsIgnoreCase("buy"))) {
-			if(signalPriceToSMA!=null && signalPriceToSMA.equalsIgnoreCase("buy") && (other.signalPriceToSMA==null || other.signalPriceToSMA.equalsIgnoreCase("put"))) {
-				return 1;
-			}
-		}
-		
-		if(signalSMAToSMA!=null && signalSMAToSMA.equalsIgnoreCase("buy") && other.signalSMAToSMA!=null && other.signalSMAToSMA.equalsIgnoreCase("buy")) {
-			if(signalPriceToSMA!=null && signalPriceToSMA.equalsIgnoreCase("buy") && other.signalPriceToSMA!=null && other.signalPriceToSMA.equalsIgnoreCase("buy")) {
-				if(percentagePriceChange>other.percentagePriceChange) {
-					return 1;
-				}
-			}
-		}
-		
-		if(signalSMAToSMA==null && other.signalSMAToSMA!=null && other.signalSMAToSMA.equalsIgnoreCase("put")) {
-			return 1;
-		}
-		
-		if(signalSMAToSMA==null && signalPriceToSMA.equalsIgnoreCase("buy") && other.signalSMAToSMA==null && (other.signalPriceToSMA == null || !other.signalPriceToSMA.equalsIgnoreCase("buy"))) {
-			return 1;
-		}
-		
-		if(signalSMAToSMA==null && signalPriceToSMA.equalsIgnoreCase("buy") && other.signalSMAToSMA==null && other.signalPriceToSMA!=null && other.signalPriceToSMA.equalsIgnoreCase("buy")) {
-			if(percentagePriceChange>other.percentagePriceChange) {
-				return 1;
-			}
-		}
-		
-		return -1;
-
-			
-
-			
-				
-
-
-		
-		//Handle buy cases
-		if(signalSMAToSMA != null && signalSMAToSMA.equalsIgnoreCase("buy") && (other.signalSMAToSMA == null || !other.signalSMAToSMA.equalsIgnoreCase("buy"))) {
-	    	//First object is first
-	    	return 1;
-	    } else if(signalSMAToSMA!=null && !signalSMAToSMA.equalsIgnoreCase("buy") && other.signalSMAToSMA!=null && other.signalSMAToSMA.equalsIgnoreCase("buy")) {
-	    	//argument object is first
-	    	return -1;
-	    } else if((signalSMAToSMA!=null && !signalSMAToSMA.equalsIgnoreCase("buy") && other.signalSMAToSMA!=null && !other.signalSMAToSMA.equalsIgnoreCase("buy")) || (signalSMAToSMA!=null && signalSMAToSMA.equalsIgnoreCase("buy") && other.signalSMAToSMA!=null && other.signalSMAToSMA.equalsIgnoreCase("buy")) ) {
-	    	//Both have buy or not buy in signalSMAToSMA
-	    	if(signalPriceToSMA!=null && signalPriceToSMA.equalsIgnoreCase("buy")  && other.signalPriceToSMA!=null && !other.signalPriceToSMA.equalsIgnoreCase("buy")) {
-	    		return 1;
-	    	} else if(signalPriceToSMA!=null && !signalPriceToSMA.equalsIgnoreCase("buy") && other.signalPriceToSMA!=null && other.signalPriceToSMA.equalsIgnoreCase("buy")) {
-	    		return -1;
-	    	} else if(signalPriceToSMA!=null && signalPriceToSMA.equalsIgnoreCase("buy") && other.signalPriceToSMA!=null && other.signalPriceToSMA.equalsIgnoreCase("buy")) {
-	    		if(percentagePriceChange>= other.percentagePriceChange) {
-	    			return 1;
-	    		} else if(percentagePriceChange< other.percentagePriceChange) {
-	    			return -1;
-	    		}
-	    	}
-	    }
-		
-		// Handle put cases
-		if(signalSMAToSMA!=null && signalSMAToSMA.equalsIgnoreCase("put") && other.signalSMAToSMA!=null && !other.signalSMAToSMA.equalsIgnoreCase("put"))	{
-			// First object is last
-			return -1;
-		}else if(signalSMAToSMA!=null && !signalSMAToSMA.equalsIgnoreCase("put") && other.signalSMAToSMA!=null && other.signalSMAToSMA.equalsIgnoreCase("put"))
-		{
-			// argument object is last
-			return 1;
-		}else if((signalSMAToSMA!=null && !signalSMAToSMA.equalsIgnoreCase("put") && other.signalSMAToSMA!=null && !other.signalSMAToSMA.equalsIgnoreCase("put")) || (signalSMAToSMA!=null && signalSMAToSMA.equalsIgnoreCase("put") && other.signalSMAToSMA!=null && other.signalSMAToSMA.equalsIgnoreCase("put"))) {
-			// Both have put or not put in signalSMAToSMA
-			if (signalPriceToSMA!=null && signalPriceToSMA.equalsIgnoreCase("put") && other.signalPriceToSMA!=null && !other.signalPriceToSMA.equalsIgnoreCase("put")) {
-				return -1;
-			} else if (signalPriceToSMA!=null && !signalPriceToSMA.equalsIgnoreCase("put") && other.signalPriceToSMA!=null && other.signalPriceToSMA.equalsIgnoreCase("put")) {
-				return 1;
-			} else if (signalPriceToSMA!=null && signalPriceToSMA.equalsIgnoreCase("put") && other.signalPriceToSMA!=null && other.signalPriceToSMA.equalsIgnoreCase("put")) {
-				if (percentagePriceChange >= other.percentagePriceChange) {
-					return -1;
-				} else if (percentagePriceChange < other.percentagePriceChange) {
-					return 1;
-				}
-			}
-		}
-	    return 0;
-	  }	*/
-	
 	
 }
