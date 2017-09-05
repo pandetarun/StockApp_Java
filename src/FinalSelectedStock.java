11b81b7e809c003d97fc5144b0a57d0c8fe73dfa
tarun.pandey <tarun.pandey@PDC2LAP-6020G31.dir.svc.accenture.com>
2017-09-05 21:59:23 +0530

    Newly added class for combined indication

---
+++ src/FinalSelectedStock.java
@@ -0,0 +1,14 @@
+import java.time.LocalDate;
+
+public class FinalSelectedStock {
+
+	String stockCode;
+	double stockPrice;
+	LocalDate tradeddate;
+	float percentagePriceChange;
+	boolean PNSMAcrossover = false;
+	boolean SMNSMcrossover = false;
+	float percentageChangeInVolumeInLastDay;
+	String BBIndicator; 
+	
+}
