d09a06e08a9e2a6a6e8452b5266d71d33568c01b
tarun.pandey <tarun.pandey@PDC2LAP-6020G31.dir.svc.accenture.com>
2017-09-20 08:30:44 +0530

    Stochastic

---
+++ src/StochasticOscillatorData.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class StochasticOscillatorData {
+	public String stockName;
+	public ArrayList<String> tradeddate;
+	public ArrayList<Float> closePrice;
+	public ArrayList<Float> highPrice;
+	public ArrayList<Float> lowPrice;
+}
